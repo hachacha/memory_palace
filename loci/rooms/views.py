f0ddd2bec486827aa2eb8caf351e996c7a0cff95
hachacha <jkiritharan@gmail.com>
2018-03-23 15:36:56 -0500

    display rooms based on pk

--- loci/rooms/views.py
+++ loci/rooms/views.py
@@ -1,6 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
-
-# Create your views here.
+from django.template import loader
 from django.http import HttpResponse
 from rooms.models import Room, Image, Image_Room_Style, Words, Words_Room_Style
 
@@ -8,4 +8,19 @@ def index(request):
 	return HttpResponse("Hello, world. You're at the rooms.")
 
 def detail(request, room_id):
-	return HttpResponse("hey you are in a particular room")
+	# template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+	try:
+		room = Room.objects.get(pk=room_id)
+		print(room)
+	except Room.DoesNotExist:
+		raise Http404("Room does not exist")
+	return render(request, 'detail.html', {'room': room})
+	# template = loader.get_template('index.html')
+	# context = {
+	# 	'room_images_list' : 
+	# }
+ #    return HttpResponse(template.render(context, request))
+	# return HttpResponse("hey you are in a particular room")
