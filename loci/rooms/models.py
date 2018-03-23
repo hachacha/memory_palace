850854019ada59a77d635c87becf2d367bef31bc
hachacha <jkiritharan@gmail.com>
2018-03-23 12:21:56 -0500

    rooms models tmp

--- loci/rooms/models.py
+++ loci/rooms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+	location = models.CharField(max_length=200)
+	created = models.DateTimeField('date published')
+	
+	def __str__(self):
+		return self.title
+
+class Image_Room_Style(models.Model):
+	room = models.ForeignKey('Room', on_delete=models.CASCADE)
+	image = models.ForeignKey('Image', on_delete=models.CASCADE)
+	width = models.IntegerField(default=0)
+
+class Room(models.Model):
+	images = models.ManyToManyField(Image)
+	created = models.DateTimeField('date published')
+	
+	def __str__(self):
+		return self.title
