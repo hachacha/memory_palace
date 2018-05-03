9edb229a57af9d0545d4a317c01a5801f20019c1
hachacha <jkiritharan@gmail.com>
2018-05-03 18:14:56 -0500

    unique together for word room styles. prevent duplicate styles for specific words in specific rooms

--- loci/rooms/models.py
+++ loci/rooms/models.py
@@ -7,7 +7,7 @@ from django.utils.crypto import get_random_string
 class Image(models.Model):
 	file = models.FileField(storage=FileSystemStorage(location=settings.MEDIA_ROOT), upload_to='images', default='settings.MEDIA_ROOT/images/')
 	created = models.DateTimeField('date published')
-	
+
 	def __str__(self):
 		return self.file.name	
 
@@ -27,10 +27,13 @@ class Words_Room_Style(models.Model):
 	words = models.ForeignKey('Words', on_delete=models.CASCADE)
 	color = models.CharField(max_length=7,default="#000000")
 
+	class Meta:
+		unique_together = ('room','words')
+
 class Room(models.Model):
-	images = models.ManyToManyField(Image)
-	text = models.ManyToManyField(Words)
-	created = models.DateTimeField('date published')
+	images = models.ManyToManyField(Image, blank=True)
+	text = models.ManyToManyField(Words, blank=True)
+	created = models.DateTimeField('date published', auto_now_add=True)
 
 class Static_Room(models.Model):
 	title = models.CharField(max_length=300) 
--- loci/rooms/tests.py
+++ loci/rooms/tests.py
@@ -1,3 +1,37 @@
+from .models import Room, Words, Words_Room_Style
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
 # Create your tests here.
+class RoomsModelsTests(TestCase):
+
+	def setUp(self):
+		#just need to be testing words and room for now
+		w = Words.objects.create(text="i am some test text")
+		w.save()
+
+		r = Room()
+		r.save()
+		r.text.add(w)
+
+
+	def test_Unique_together_gives_validation_error_on_not_unique(self):
+		wrs = Words_Room_Style()
+		duplicate_wrs = Words_Room_Style()
+		
+		wrs.color="purple"
+		wrs.room=Room.objects.get(pk=1)
+		wrs.words=Words.objects.get(pk=1)
+		wrs.save()
+		
+		duplicate_wrs.color="red"
+		duplicate_wrs.room=Room.objects.get(pk=1)
+		duplicate_wrs.words=Words.objects.get(pk=1)
+
+		try:
+			duplicate_wrs.save()
+			print("integrity error did not fire")
+			self.assertTrue(0)
+		except IntegrityError:
+			print("integrity error fired correctly")
+			self.assertTrue(1)
