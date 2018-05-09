96921a7b79b4a86e9664e7af8bdc3ca6fd6a9e10
hachacha <jkiritharan@gmail.com>
2018-05-08 23:25:43 -0500

    rangefields to image style and word style

--- loci/rooms/admin.py
+++ loci/rooms/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import Room, Image, Image_Room_Style, Words, Words_Room_Style
+from .models import Room, Image, Image_Style, Words, Words_Style
 # Register your models here.
 
 
 admin.site.register(Image)
 admin.site.register(Room)
 admin.site.register(Words)
-admin.site.register(Image_Room_Style)
-admin.site.register(Words_Room_Style)
+admin.site.register(Image_Style)
+admin.site.register(Words_Style)
--- loci/rooms/models.py
+++ loci/rooms/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from django.utils.crypto import get_random_string
+from django.contrib.postgres.fields import IntegerRangeField, FloatRangeField
+from psycopg2.extras import NumericRange
+from django.contrib.postgres.validators import RangeMinValueValidator, RangeMaxValueValidator
 
-# Create your models here.
+
 class Image(models.Model):
 	file = models.FileField(storage=FileSystemStorage(location=settings.MEDIA_ROOT), upload_to='images', default='settings.MEDIA_ROOT/images/')
 	created = models.DateTimeField('date published')
@@ -11,11 +14,127 @@ class Image(models.Model):
 	def __str__(self):
 		return self.file.name	
 
+
+class Image_Style(models.Model):
+	image = models.ForeignKey('Image', on_delete=models.CASCADE)
+	width = IntegerRangeField(default='(1, 101)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(100)
+								]
+				)
+	height = IntegerRangeField(default='(1,101)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(100)
+								]
+				)
+	border_radius_tl = IntegerRangeField(default='(1,101)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(100)
+								]
+					)
+	border_radius_tr =IntegerRangeField(default='(1,101)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(100)
+								]
+					)
+	border_radius_bl = IntegerRangeField(default='(1,101)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(100)
+								]
+					)
+	border_radius_br = IntegerRangeField(default='(1,101)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(100)
+								]
+					)
+	opacity = FloatRangeField(default='(0.1, 1.1)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(0.1), 
+									RangeMaxValueValidator(1.1)
+								]
+					)
+	marquee = models.BooleanField(default=False)#should look this up...? maybe set up a function to do so.and automatically tie it to the IRS.
+
+
 class Words(models.Model):
 	text = models.TextField()
 
 	def __str__(self):
 		return self.text
+
+	
+class Words_Style(models.Model):
+	words = models.ForeignKey('Words', on_delete=models.CASCADE)
+	color = IntegerRangeField(default='(0, 255)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(0), 
+									RangeMaxValueValidator(255)
+								]
+					)
+	size = IntegerRangeField(default='(1, 10)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(10)
+								]
+					)
+	style = models.CharField(max_length=8, default='')
+	marquee = models.BooleanField(default=False)
+
+class Gen_Room_Style(models.Model):
+	background_image = models.FileField(storage=FileSystemStorage(location=settings.MEDIA_ROOT), upload_to='images', default='settings.MEDIA_ROOT/images/')
+	background_image_format = models.CharField(max_length=16,default='')
+
+# class Div_Room_Style(models.Model):
+# 	pass
+
+class Marquee_Style(models.Model):
+	image = models.ForeignKey('Image',on_delete=models.CASCADE, blank=True)
+	words = models.ForeignKey('Words',on_delete=models.CASCADE, blank=True)
+	behavior = models.CharField(max_length=24, default="scroll")
+	direction = models.CharField(max_length=18,default="left")
+	scrollamount = IntegerRangeField(default='(1, 5)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(5)
+								]
+					)
+	scrolldelay = IntegerRangeField(default='(1, 5)',
+								 blank=True,
+								 validators=[
+									RangeMinValueValidator(1), 
+									RangeMaxValueValidator(5)
+								]
+					)
+
+class Room(models.Model):
+	images = models.ManyToManyField(Image, blank=True)
+	text = models.ManyToManyField(Words, blank=True)
+	# styles = models.ForeignKey('Gen_Room_Style', on_delete=models.CASCADE, blank=True, default=None)
+	created = models.DateTimeField('date published', auto_now_add=True)
+
+class Static_Room(models.Model):
+	title = models.CharField(max_length=300) 
+	url = models.CharField(max_length=350)
+
+
+
+
 
 class Image_Room_Style(models.Model):
 	room = models.ForeignKey('Room', on_delete=models.CASCADE)
@@ -40,28 +159,3 @@ class Words_Room_Style(models.Model):
 
 	class Meta:
 		unique_together = ('room','words')
-
-class Gen_Room_Style(models.Model):
-	background_image = models.FileField(storage=FileSystemStorage(location=settings.MEDIA_ROOT), upload_to='images', default='settings.MEDIA_ROOT/images/')
-	background_image_format = models.CharField(max_length=16,default='')
-
-# class Div_Room_Style(models.Model):
-# 	pass
-
-
-class Marquee_Style(models.Model):
-	image = models.ForeignKey('Image',on_delete=models.CASCADE, blank=True)
-	words = models.ForeignKey('Words',on_delete=models.CASCADE, blank=True)
-
-	behavior = models.CharField(max_length=24, default="scroll")
-	direction = models.CharField(max_length=18,default="left")
-
-class Room(models.Model):
-	images = models.ManyToManyField(Image, blank=True)
-	text = models.ManyToManyField(Words, blank=True)
-	# styles = models.ForeignKey('Gen_Room_Style', on_delete=models.CASCADE, blank=True, default=None)
-	created = models.DateTimeField('date published', auto_now_add=True)
-
-class Static_Room(models.Model):
-	title = models.CharField(max_length=300) 
-	url = models.CharField(max_length=350)
